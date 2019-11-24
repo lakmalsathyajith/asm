7551d900a9e0991512bc7b0d516e36c3e403db5a
Harsha Sampath <harsha@listudiosl.com>
2019-11-24 19:57:52 +0530

    removed swiper from vuex

--- resources/assets/js/store/modules/ratesAndAvailability/mutations.js
+++ resources/assets/js/store/modules/ratesAndAvailability/mutations.js
@@ -10,58 +10,8 @@ export default {
   },
   [GET_APARTMENTS_LIST](state, payload) {
     state.apartmentsList = payload.data;
-    setTimeout(() => {
-      if ($('.swiper-container').length) {
-        var swiper = new Swiper('.swiper-container', {
-          effect: 'fade',
-          fadeEffect: {
-            crossFade: true
-          },
-          loop: true,
-          speed: 1000,
-          centeredSlides: true,
-          autoplay: {
-            delay: 3000,
-            disableOnInteraction: false
-          },
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          },
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-          }
-        });
-      }
-    }, 1000);
   },
   [SELECTED_APARTMENT](state, payload) {
     state.selectedApartment = payload;
-    setTimeout(() => {
-        if ($('.swiper-container').length) {
-          var swiper = new Swiper('.swiper-container', {
-            effect: 'fade',
-            fadeEffect: {
-              crossFade: true
-            },
-            loop: true,
-            speed: 1000,
-            centeredSlides: true,
-            autoplay: {
-              delay: 3000,
-              disableOnInteraction: false
-            },
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true
-            },
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev'
-            }
-          });
-        }
-      }, 1000);
   }
 };
