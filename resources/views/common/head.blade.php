71b7f22757600629b0b6dba6a5ccea556b94c12a
Harsha Sampath <harsha@listudiosl.com>
2020-01-20 17:45:05 +0530

    share issues fixed

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -2,25 +2,40 @@
     <title>Apartment Stays Melbourne</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name=viewport content="width=device-width, initial-scale=1">
-    <link rel="shortlink" href="" />
-    <link rel="canonical" href="" />
+    <link rel="shortlink" href="{{url()->current()}}" />
+    <link rel="canonical" href="{{url()->current()}}" />
 
     <!-- seo tags -->
     <meta property="og:locale" content="en_US" />
-    <meta property="og:image" content="{{asset('images/share-image.jpg')}}" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="- Apartment Stays Melbourne" />
-    <meta property="og:description" content="" />
-    <meta property="og:url" content="" />
+    <meta property="og:url" content="{{url()->current()}}" />
     <meta property="og:site_name" content="Apartment Stays Melbourne" />
     <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content="- Apartment Stays Melbourne" />
-    @if(isset($meta))
-        <meta name="description" content="{{$meta['description']}}" />
-        <meta name="keywords" content="{{$meta['keywords']}}" />
+
+
+    @if(isset($meta['image']))
+    <meta property="og:image" content="{{$meta['image']}}" />
     @else
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
+    <meta property="og:image" content="{{asset('images/share-image.jpg')}}" />
+    @endif
+
+    @if(isset($meta['title']))
+    <meta property="og:title" content="{{$meta['title']}}" />
+    <meta name="twitter:title" content="{{$meta['title']}}" />
+    @else
+    <meta property="og:title" content="Apartment Stays Melbourne" />
+    <meta name="twitter:title" content="Apartment Stays Melbourne" />
+
+    @endif
+
+    @if(isset($meta))
+    <meta name="description" content="{{$meta['description']}}" />
+    <meta property="og:description" content="{{$meta['description']}}" />
+    <meta name="keywords" content="{{$meta['keywords']}}" />
+    @else
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta property="og:description" content="" />
     @endif
     <meta name="robots" content="index, follow" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -46,5 +61,6 @@
     <link rel="stylesheet" href="{{asset('css/responsive.css')}}" />
     <link rel="stylesheet" href="{{asset('css/swipper.css')}}" />
     <link rel="stylesheet" href="{{asset('css/lightbox.min.css')}}" />
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
